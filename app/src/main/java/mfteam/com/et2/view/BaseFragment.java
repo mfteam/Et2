e1b238973ec2478f6d6a9a37e17ddd902809cb69
erdemisguder <erdem.isguder@rdc.com.tr>
2017-06-06 18:12:29 +0300

    [Redugsi]Databinding is ready <31

---
+++ app/src/main/java/mfteam/com/et2/view/BaseFragment.java
@@ -0,0 +1,11 @@
+package mfteam.com.et2.view;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by redugsi on 06/06/17.
+ */
+
+public class BaseFragment extends Fragment {
+
+}
---
+++ app/src/main/java/mfteam/com/et2/viewmodel/BaseViewModel.java
@@ -0,0 +1,8 @@
+package mfteam.com.et2.viewmodel;
+
+/**
+ * Created by redugsi on 06/06/17.
+ */
+
+public class BaseViewModel {
+}
